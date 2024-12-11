969fb6f43139d07a334ceb48afd767e8d6597ca5
ddavletshin <dinar24121998@mail.ru>
2024-12-11 19:07:52 +0300

    chore(configs): install packages and update configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,21 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
     { ignores: ['dist'] },
     {
-        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommended,
+            eslintPluginPrettierRecommended,
+            eslintConfigPrettier,
+        ],
         files: ['**/*.{ts,tsx}'],
         languageOptions: {
             ecmaVersion: 2020,
@@ -16,13 +24,26 @@ export default tseslint.config(
         plugins: {
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
+            'simple-import-sort': simpleImportSort,
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    caughtErrors: 'none',
+                },
+            ],
+            'object-curly-spacing': ['error', 'always'],
+            'prettier/prettier': ['warn'],
+            quotes: ['error', 'single'],
             'react-refresh/only-export-components': [
                 'warn',
                 { allowConstantExport: true },
             ],
+            semi: ['error', 'never'],
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
         },
     },
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import * as path from 'node:path'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(), svgr()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    server: {
+        proxy: {
+            '/api': {
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+                target: 'http://localhost:9002/',
+            },
+        },
+    },
 })
